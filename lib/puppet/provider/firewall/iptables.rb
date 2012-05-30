6d6243905df13e87fb94e3b1d55d073fac4345e7
Jack Neely <jjneely@ncsu.edu>
2012-05-30 16:37:18 -0400

    Fine tune regex for deleting rules
    Its possible that the /-A/-D/ regex can match other things such as local
    chains in my case.  Let's make this regex here a little more specific.

--- lib/puppet/provider/firewall/iptables.rb
+++ lib/puppet/provider/firewall/iptables.rb
@@ -194,7 +194,7 @@ Puppet::Type.type(:firewall).provide :iptables, :parent => Puppet::Provider::Fir
 
   def delete_args
     count = []
-    line = properties[:line].gsub(/\-A/, '-D').split
+    line = properties[:line].gsub(/\s+\-A\s+/, ' -D ').split
 
     # Grab all comment indices
     line.each do |v|
